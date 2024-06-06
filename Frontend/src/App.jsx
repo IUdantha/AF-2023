ec2f1eb597023f62f9d23c679c16d3a3e7b93a6e
Udantha Isuru <udanthaisuru@gmail.com>
2024-06-07 04:25:07 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import Cart from "./pages/Cart";
 import BookDetails from "./pages/BookDetails";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/bookDetails/:id" element={<BookDetails />} />
       </Routes>
     </BrowserRouter>
--- Frontend/src/pages/cart.jsx
+++ Frontend/src/pages/cart.jsx
@@ -2,7 +2,48 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Cart() {
-  return <></>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5050/purchases")
+      .then((response) => {
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h3>Your Orders</h3>
+      <table>
+        <thead>
+          <tr>
+            <td>PurchaseID</td>
+            <td>BookID</td>
+            <td>Title</td>
+            <td>Author</td>
+            <td>Price</td>
+            <td>Genre</td>
+          </tr>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr key={index}>
+                <td>{order.purchaseId}</td>
+                <td>{order.id}</td>
+                <td>{order.title}</td>
+                <td>{order.author}</td>
+                <td>{order.price}</td>
+                <td>{order.genre}</td>
+              </tr>
+            ))}
+          </tbody>
+        </thead>
+      </table>
+    </>
+  );
 }
 
 export default Cart;
